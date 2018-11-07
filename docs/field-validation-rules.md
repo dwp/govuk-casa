5a4e01ae9256cd4873a1dc45f7777d4f2e463df2
Dan Boscaro <dan.boscaro@engineering.digital.dwp.gov.uk>
2018-11-07 12:48:15 +0000

    feat(validation): update nino validation rule with allowWhitespace flag
    Signed-off-by: Dan Boscaro <dan.boscaro@engineering.digital.dwp.gov.uk>

--- docs/field-validation-rules.md
+++ docs/field-validation-rules.md
@@ -8,6 +8,49 @@
 
 ## `nino`
 
+```javascript
+rules.nino
+```
+
+By default the nino rule will check a field value is in the correct format, without spaces and is case insensitive. 
+
+e.g. `AA000000A` and `aa000000a` will pass.
+
+But `AA 00 00 00 A` will fail with the message `Enter a valid National Insurance number`  
+
+---
+
+### Binding options 
+
+`allowWhitespace`: boolean
+
+Strips all space characters from the field value before checking the format. e.g. `AA 00 00 00 A` will pass
+
+```javascript
+rules.nino.bind({
+    allowWhitespace: true
+})
+```
+
+Throws TypeError if incorrect
+
+---
+
+`errMsg`: string or object
+
+Overrides the default error messages when the field value fails validation.
+
+```javascript
+rules.nino.bind({
+    errMsg: {
+        summary: 'Custom message to appear in the error summary',
+        inline: 'Custom message to appear next to the nino field'
+    }
+})
+```
+
+When supplied as a string, the same message is used for both the `inline` and `summary`
+
 ## `optional`
 
 ## `postalAddressObject`
--- test/unit/validation-rules/ninoTests.js
+++ test/unit/validation-rules/ninoTests.js
@@ -28,4 +28,42 @@ describe('Validation rule: nino', () => {
 
     return Promise.all(queue);
   });
+
+  it('should reject valid NI numbers with spaces by default', () => {
+    const queue = [];
+
+    queue.push(expect(nino('AA 00 00 00 A')).to.be.rejected);
+    queue.push(expect(nino('AA\u002000\u002000\u002000\u0020A')).to.be.rejected);
+    queue.push(expect(nino('JJ 123456D ')).to.be.rejected);
+
+    return Promise.all(queue);
+  });
+
+  it('should accept valid NI numbers with spaces when allowWhitespace is true', () => {
+    const rule = nino.bind({
+      allowWhitespace: true
+    });
+    const queue = [];
+
+    queue.push(expect(rule('AA 00 00 00 A')).to.be.fulfilled);
+    queue.push(expect(rule('JJ 123456D ')).to.be.fulfilled);
+    queue.push(expect(rule('JJ 123  456D')).to.be.fulfilled);
+    queue.push(expect(rule('AA\u002000\u002000\u002000\u0020A')).to.be.fulfilled);
+
+    return Promise.all(queue);
+  });
+
+  it('should reject valid NI numbers with non standard spaces when allowWhitespace is true', () => {
+    const rule = nino.bind({
+      allowWhitespace: true
+    });
+    return expect(rule('AA\u200200\u200200\u200200\u2002A')).to.be.rejected;
+  });
+
+  it('should throw TypeError when allowWhitespace isnt a boolean', () => {
+    const rule = nino.bind({
+      allowWhitespace: 'true'
+    });
+    return expect(() => rule('AA 00 00 00 A')).to.throw(TypeError, 'NINO validation rule option "allowWhitespace" must been a boolean. received string');
+  });
 });
