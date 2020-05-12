2dc23f0793d5f20f050b6f99517eacf331940262
james.gauld <james.gauld@engineering.digital.dwp.gov.uk>
2020-05-12 10:29:09 +0100

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-  "root": true,
-  "extends": ["@dwp/eslint-config-base", "plugin:sonarjs/recommended"],
-  "plugins": [
-    "sonarjs"
+  root: true,
+  extends: ['@dwp/eslint-config-base', 'plugin:sonarjs/recommended'],
+  plugins: [
+    'sonarjs',
   ],
-  "rules": {
-    "indent": [2, 2],
-    "semi": [0],
-    "sonarjs/cognitive-complexity": [1, 10]
+  rules: {
+    indent: [2, 2],
+    semi: [0],
+    'sonarjs/cognitive-complexity': [1, 10],
   },
-  "globals": {
-    "GOVUK_CASA_DEBUG_NS": true
+  globals: {
+    GOVUK_CASA_DEBUG_NS: true,
   },
-  "parserOptions": {
-    "ecmaVersion": "2018"
-  }
+  parserOptions: {
+    ecmaVersion: '2018',
+  },
 };
