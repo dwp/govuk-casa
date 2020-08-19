219734c290cc6b0607e25794087043fed9e39a64
Steve Clements <stephen.clements@engineering.digital.dwp.gov.uk>
2020-08-19 09:28:06 +0100

    chore(typescript): update ValidationRules namespace

--- lib/validation/rules/ValidationRules.d.ts
+++ lib/validation/rules/ValidationRules.d.ts
@@ -1,7 +1,13 @@
 import ValidationError from '../../../lib/validation/ValidationError';
 
 export namespace ValidationRules {
-  export function email(): Promise<ValidationError>;
-  export function required(): Promise<ValidationError>;
-  export function inArray(): Promise<ValidationError>;
+  export function dateObject(): Promise<ValidationError | void>;
+  export function email(): Promise<ValidationError | void>;
+  export function inArray(): Promise<ValidationError | void>;
+  export function nino(): Promise<ValidationError | void>;
+  export function optional(): boolean;
+  export function postalAddressObject(): Promise<ValidationError | void>;
+  export function regex(): Promise<ValidationError | void>;
+  export function required(): Promise<ValidationError | void>;
+  export function strlen(): Promise<ValidationError | void>;
 }
