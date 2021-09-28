ec95b9193e7f8057be9af34031fb4b61ce89902a
james.gauld <james.gauld@engineering.digital.dwp.gov.uk>
2021-09-28 08:33:24 +0100

    chore: lint adustments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,26 @@
 module.exports = {
-  "root": true,
-  "extends": ["@dwp/eslint-config-base", "plugin:sonarjs/recommended"],
-  "plugins": [
-    "sonarjs"
+  root: true,
+  extends: ['@dwp/eslint-config-base', 'plugin:sonarjs/recommended'],
+  plugins: [
+    'sonarjs',
   ],
-  "rules": {
-    "indent": [2, 2],
-    "semi": [0],
-    "sonarjs/cognitive-complexity": [1, 10]
+  rules: {
+    indent: [2, 2],
+    semi: [0],
+    'sonarjs/cognitive-complexity': [1, 10],
+
+    // Maintenance update to eslint introdues these
+    'jsdoc/require-throws': 0,
+    'jsdoc/require-description-complete-sentence': 0,
+    'jsdoc/check-tag-names': 0,
+    'jsdoc/require-returns': 0,
+    'jsdoc/require-param': 0,
+    'jsdoc/require-description': 0,
   },
-  "globals": {
-    "GOVUK_CASA_DEBUG_NS": true
+  globals: {
+    GOVUK_CASA_DEBUG_NS: true,
   },
-  "parserOptions": {
-    "ecmaVersion": "2018"
-  }
+  parserOptions: {
+    ecmaVersion: '2018',
+  },
 };
