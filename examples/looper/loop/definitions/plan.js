b43f40124ec7d74112e4871fac87661a28b5c8d6
james.gauld <james.gauld@engineering.digital.dwp.gov.uk>
2022-06-01 12:10:11 +0100

    chore: remove looper example
    This will be put in the looper plugin repo itself.

--- examples/looper/loop/definitions/plan.js
+++
@@ -1,9 +0,0 @@
-import { Plan } from '@dwp/govuk-casa';
-
-export default () => {
-  const plan = new Plan();
-
-  plan.addSequence('hobby', 'location', 'check-your-hobby-answers');
-
-  return plan;
-};
