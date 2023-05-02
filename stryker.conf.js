936b02b859e7658fa27ea3814bf4be78649d9ca1
james.gauld <james.gauld@engineering.digital.dwp.gov.uk>
2023-05-02 13:54:45 +0100

    chore: update stryker

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,28 +1,27 @@
-module.exports = (config) => {
-  config.set({
-    mutator: 'javascript',
-    packageManager: 'npm',
-    reporters: ['clear-text', 'progress', 'html'],
-    htmlReporter: {
-      baseDir: '.coverage/stryker',
-    },
-    testRunner: 'mocha',
-    transpilers: [],
-    testFramework: 'mocha',
-    coverageAnalysis: 'all',
-    mutate: [
-      'lib/**/*.js',
-      'middleware/**/*.js',
+module.exports = {
+  mutator: 'javascript',
+  packageManager: 'npm',
+  reporters: ['clear-text', 'progress', 'html'],
+  htmlReporter: {
+    baseDir: '.coverage/stryker',
+  },
+  testRunner: 'mocha',
+  transpilers: [],
+  testFramework: 'mocha',
+  coverageAnalysis: 'perTest',
+  mutate: [
+    'lib/**/*.js',
+    'middleware/**/*.js',
+  ],
+  mochaOptions: {
+    spec: [
+      'test/unit/**/*.test.js',
     ],
-    mochaOptions: {
-      spec: [
-        'test/unit/**/*.test.js',
-      ],
-    },
-    thresholds: {
-      high: 80,
-      low: 70,
-      break: 60,
-    },
-  });
+  },
+  thresholds: {
+    high: 80,
+    low: 70,
+    break: 60,
+  },
+  ignoreStatic: true,
 };
