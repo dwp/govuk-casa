f18418c282516b7cd56c00b1515bf61bcfdd43f2
Lewis Jones <lewis.jones@engineering.digital.dwp.gov.uk>
2023-05-16 13:23:26 +0100

    feat: use csrf-sync package for csrf
    Pre-Commit-Config: 58587b58701d03ea2c6f5a543763bc0529b2c7e23b4bae3198ae9de6493a184a

--- src/middleware/csrf.js
+++ src/middleware/csrf.js
@@ -1,23 +1,15 @@
-import csurf from 'csurf';
+import { csrfSync } from 'csrf-sync';
 
 // 2 middleware: one to generate the csrf token and check its validity (POST
 // only), and one to provide that token to templates via the `casa.csrfToken`
 // variable.
 
 export default function csrfMiddleware() {
+  const { csrfSynchronisedProtection } = csrfSync({
+    getTokenFromRequest: (req) => req.body._csrf,
+  });
   return [
-    csurf({
-      cookie: false,
-      sessionKey: 'session',
-      // value: (req) => {
-      //   // Here we clear the token after extracting to maintain cleaner data. It
-      //   // is only used for this CSRF purpose.
-      //   const token = String(req.body._csrf);
-      //   delete req.body._csrf;
-      //   return token;
-      //   /* eslint-enable no-underscore-dangle */
-      // },
-    }),
+    csrfSynchronisedProtection,
     (req, res, next) => {
       res.locals.casa = {
         ...res.locals?.casa,
---
+++ tests/middleware/csrf.test.js
@@ -0,0 +1,54 @@
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import chai, { expect } from 'chai';
+import ExpressJS from 'express';
+import request from 'supertest';
+import session from 'express-session';
+
+import csrfMiddleware from '../../src/middleware/csrf.js';
+
+chai.use(sinonChai);
+
+describe('csrf middleware', () => {
+  it('passes through to next middleware', (done) => {
+    // csrf-sync requires an application with session storage
+    const app = ExpressJS();
+    app.use(session({
+      secret: 'test',
+      resave: false,
+      saveUninitialized: true,
+    }));
+    app.use(csrfMiddleware());
+    app.use((req, res) => {
+      res.status(200).send('passthrough');
+    });
+
+    request(app)
+      .get('/')
+      .expect((res) => expect(res.text).to.equal('passthrough'))
+      .end(done)
+  });
+
+  it('adds csrfToken to res.locals.casa', () => {
+    const [, middleware] = csrfMiddleware();
+    const csrfToken = '12345';
+    const req = {
+      body: {
+        _csrf: csrfToken,
+      },
+      session: {
+        csrfToken,
+      },
+      csrfToken: () => csrfToken,
+    };
+    const res = {
+      locals: {},
+    };
+    const next = sinon.stub();
+
+    middleware(req, res, next);
+
+    expect(res.locals.casa.csrfToken).to.equal(csrfToken);
+    expect(next).to.be.calledOnce;
+  });
+});
