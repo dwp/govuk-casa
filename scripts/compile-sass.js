06ad4d1f40832244965232d5b1c4e8e2a30863c6
Niall Molloy <niall.molloy@engineering.digital.dwp.gov.uk>
2023-10-27 13:54:06 +0100

    feat: serve scss/css source maps

--- scripts/compile-sass.js
+++ scripts/compile-sass.js
@@ -19,15 +19,27 @@ async function compileSassSources() {
   const govukFrontendDirectory = resolve(require.resolve('govuk-frontend'), '../../');
 
   // Main CSS
-  const { css } = compile('assets/scss/casa.scss', {
+  const { css, sourceMap } = compile('assets/scss/casa.scss', {
     loadPaths: [
       govukFrontendDirectory,
     ],
     style: 'compressed',
+    sourceMap: true,
     quietDeps: true,
   });
 
-  await writeFile('dist/assets/css/casa.css', css, { encoding: 'utf8' });
+  // Rewrite source map paths from absolute project path URLs to paths relative to
+  // the compiled css file.
+  for (let i = 0; i < sourceMap.sources.length; i++) {
+    sourceMap.sources[i] = sourceMap.sources[i]
+      .replace(`file://${govukFrontendDirectory}/`, '../../../');
+  }
+
+  const targetFile = 'dist/assets/css/casa.css';
+  await Promise.all([
+    writeFile(targetFile, `${css}\n/*# sourceMappingURL=casa.css.map */`, { encoding: 'utf8' }),
+    writeFile(`${targetFile}.map`, JSON.stringify(sourceMap), { encoding: 'utf8' }),
+  ]);
 }
 
 await compileSassSources();
